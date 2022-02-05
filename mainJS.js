22396cb8292b9e10b5a7483c8bd853f245f78905
ignasc <ignui.c@gmail.com>
2022-02-05 17:12:16 +0000

    Trying to figure out provided data.

--- mainJS.js
+++ mainJS.js
@@ -6,23 +6,93 @@ const USCountryData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/c
 
 document.addEventListener("DOMContentLoaded", function(){
 
-       const req1 = new XMLHttpRequest();
-       req1.open("GET",USEducationData,true);
-       req1.send();
-       req1.onload = function(){
-              const jsonEducation = JSON.parse(req1.responseText);
-              console.log("First link loaded");
-              console.log(jsonEducation);
-       };
-       const req2 = new XMLHttpRequest();
-       req2.open("GET",USCountryData,true);
-       req2.send();
-       req2.onload = function(){
-              const jsonCountry = JSON.parse(req2.responseText);
-              console.log("Second link loaded");
-              console.log(jsonCountry);
+       const req = new XMLHttpRequest();
+       req.open("GET",USEducationData,true);
+       req.send();
+       req.onload = function(){
+              const jsonEducation = JSON.parse(req.responseText);
+
+              req.open("GET",USCountryData,true);
+              req.send();
+              req.onload = function(){
+                     const jsonCountry = JSON.parse(req.responseText);
+
+                     main(jsonEducation, jsonCountry);
+
+              };
        };
 
 
 
 });
+
+function main(jsonEducation, jsonCountry){
+       
+       /*Set up SVG canvas to work on*/
+       const pageWidth  = document.documentElement.scrollWidth;
+       const chartWidth = pageWidth<1024? 1024:Math.round(pageWidth*0.8);
+       const chartHeight = 800;
+       const padding = 60;
+
+       const svg = d3.select("#choropleth-map");
+
+       svg.attr("width", chartWidth)
+              .attr("height", chartHeight)
+              /*center svg element*/
+              .style("margin-left", (d)=>{
+                     if(pageWidth - chartWidth <=0) {
+                            return 0 + "px";
+                     };
+                     return (pageWidth - chartWidth)/2 + "px";
+              })
+              .style("margin-top", "20px");
+
+       
+
+       /*debug section*/
+
+       const tempTestScaleX = d3.scaleLinear()
+              .domain([
+                     d3.min(jsonCountry.arcs, (d)=>{
+                            return d[0][0];
+                     }),
+                     d3.max(jsonCountry.arcs, (d)=>{
+                            return d[0][0];
+                     })
+              ])
+              .range([
+                     padding,chartWidth - padding
+              ]);
+
+       const tempTestScaleY = d3.scaleLinear()
+              .domain([
+                     d3.max(jsonCountry.arcs, (d)=>{
+                            return d[0][1];
+                     }),
+                     d3.min(jsonCountry.arcs, (d)=>{
+                            return d[0][1];
+                     })
+              ])
+              .range([
+                     chartHeight - padding, padding
+              ]);
+
+       svg.selectAll("circle")
+              .data(jsonCountry.arcs)
+              .enter()
+              .append("circle")
+              .attr("id","country-circle")
+              .attr("r", 2)
+              .attr("cx",
+                     (d)=>tempTestScaleX(d[0][0])
+              )
+              .attr("cy",
+                     (d)=>tempTestScaleY(d[0][1])
+              );
+
+       console.log("jsonEducation");
+       console.log(jsonEducation);
+       console.log("jsonCountry");
+       console.log(jsonCountry.arcs[0][0]);
+
+};
