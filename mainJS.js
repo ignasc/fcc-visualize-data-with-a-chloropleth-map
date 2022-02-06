4bf9c6a756858a711ab2a98d74989d46ffc22d6a
ignasc <ignui.c@gmail.com>
2022-02-06 20:45:43 +0000

    Just some experimenting with data.
    Until I realized I need to use  topojson extension.

--- mainJS.js
+++ mainJS.js
@@ -2,7 +2,7 @@
 /*https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json*/
 
 const USEducationData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
-const USCountryData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+const USCountyData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -12,12 +12,28 @@ document.addEventListener("DOMContentLoaded", function(){
        req.onload = function(){
               const jsonEducation = JSON.parse(req.responseText);
 
-              req.open("GET",USCountryData,true);
+              req.open("GET",USCountyData,true);
               req.send();
               req.onload = function(){
-                     const jsonCountry = JSON.parse(req.responseText);
+                     const jsonCounty = JSON.parse(req.responseText);
 
-                     main(jsonEducation, jsonCountry);
+                     /*reduced size arrays (temporary)*/
+                     let counter = 100;
+                     let tempJsonEducation = [];
+                     let tempJsonCounty = {arcs: []};
+
+                     for(let i = 0; i<counter; i++){
+                            tempJsonEducation.push(jsonEducation[i]);
+
+                            tempJsonCounty.arcs.push(jsonCounty.arcs[i]);
+                     };
+                     
+                     console.log("tempJsonEducation");
+                     console.log(tempJsonEducation);
+                     console.log("tempJsonCountry");
+                     console.log(tempJsonCounty);
+
+                     main(tempJsonEducation, tempJsonCounty);
 
               };
        };
@@ -26,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function(){
 
 });
 
-function main(jsonEducation, jsonCountry){
+function main(jsonEducation, jsonCounty){
        
        /*Set up SVG canvas to work on*/
        const pageWidth  = document.documentElement.scrollWidth;
@@ -53,10 +69,10 @@ function main(jsonEducation, jsonCountry){
 
        const tempTestScaleX = d3.scaleLinear()
               .domain([
-                     d3.min(jsonCountry.arcs, (d)=>{
+                     d3.min(jsonCounty.arcs, (d)=>{
                             return d[0][0];
                      }),
-                     d3.max(jsonCountry.arcs, (d)=>{
+                     d3.max(jsonCounty.arcs, (d)=>{
                             return d[0][0];
                      })
               ])
@@ -66,10 +82,10 @@ function main(jsonEducation, jsonCountry){
 
        const tempTestScaleY = d3.scaleLinear()
               .domain([
-                     d3.max(jsonCountry.arcs, (d)=>{
+                     d3.max(jsonCounty.arcs, (d)=>{
                             return d[0][1];
                      }),
-                     d3.min(jsonCountry.arcs, (d)=>{
+                     d3.min(jsonCounty.arcs, (d)=>{
                             return d[0][1];
                      })
               ])
@@ -78,11 +94,11 @@ function main(jsonEducation, jsonCountry){
               ]);
 
        svg.selectAll("circle")
-              .data(jsonCountry.arcs)
+              .data(jsonCounty.arcs)
               .enter()
               .append("circle")
               .attr("id","country-circle")
-              .attr("r", 2)
+              .attr("r", 1)
               .attr("cx",
                      (d)=>tempTestScaleX(d[0][0])
               )
@@ -90,9 +106,61 @@ function main(jsonEducation, jsonCountry){
                      (d)=>tempTestScaleY(d[0][1])
               );
 
+       svg.selectAll("path")
+              .data(jsonCounty.arcs)
+              .enter()
+              .append("path")
+              .style("stroke", "red")
+              .style("fill", "none")
+              .attr("d", (d)=>{
+                     let startCoordinate = tempTestScaleX(d[0][0]) + " " + tempTestScaleY(d[0][1]);
+
+                     let testArrayLOL = [];
+                     let relativePoints = "";
+
+                     d.forEach((item, index)=>{
+                            if(index==0){return};
+                            /*
+                            patikrinti, ar skaicius neigiamas
+                            jei neigiamas, nuimti minuso zenkla
+                            scale the number to proper size
+                            prideti minuso zenkla, jei buvo jis nuimtas
+                            prideti koordinate
+                            */
+
+                            testArrayLOL.push(scaleTheCoordinates(item, tempTestScaleX, tempTestScaleY));
+                     })
+
+                     testArrayLOL.forEach((d)=>{
+                            relativePoints = relativePoints + "l" + d[0] + " " + d[1] + " ";
+                     })
+
+                     /*return "M " + startCoordinate + relativePoints + "z"; */
+                     return "M " + startCoordinate + "l20 0 l 0 -20" + "z";
+              });
+
        console.log("jsonEducation");
        console.log(jsonEducation);
        console.log("jsonCountry");
-       console.log(jsonCountry.arcs[0][0]);
+       console.log(jsonCounty);
 
+};
+
+function scaleTheCoordinates(coordinateArray, scaleX, scaleY){
+       let negNumberX = true;
+       let negNumberY = true;
+       let scaledNumberX;
+       let scaledNumberY;
+       if(coordinateArray[0]<0){
+              negNumberX = false;
+       }
+       if(coordinateArray[1]<0){
+              negNumberY = false;
+       }
+
+       scaledNumberX = negNumberX===true?scaleX(Math.abs(coordinateArray[0]))*(-1):scaleX(coordinateArray[0]);
+
+       scaledNumberY = negNumberY===true?scaleY(Math.abs(coordinateArray[1]))*(-1):scaleY(coordinateArray[1]);
+
+       return [scaledNumberX, scaledNumberY];
 };
