5092d98339ed8064104c521a6dd57c9bb26d5745
ignasc <ignui.c@gmail.com>
2022-02-12 18:11:55 +0000

    Fixed map generation using d3.geoPath
    Which is the proper way to generate this map.

--- mainJS.js
+++ mainJS.js
@@ -8,6 +8,12 @@ const USCountyData = "https://cdn.freecodecamp.org/testable-projects-fcc/data/ch
 /*LOCAL DATA FOR DEVELOPMENT*/
 const USEducationData = "./data/for_user_education.json";
 const USCountyData = "./data/counties.json";
+       
+/*Set up SVG canvas to work on*/
+const pageWidth  = document.documentElement.scrollWidth;
+const chartWidth = 1024;
+const chartHeight = 800;
+const padding = 60;
 
 document.addEventListener("DOMContentLoaded", function(){
 
@@ -26,18 +32,15 @@ document.addEventListener("DOMContentLoaded", function(){
 
               };
        };
-
-
-
 });
 
 function main(jsonEducation, jsonCounty){
-       
-       /*Set up SVG canvas to work on*/
-       const pageWidth  = document.documentElement.scrollWidth;
-       const chartWidth = pageWidth<1024? 1024:Math.round(pageWidth*0.8);
-       const chartHeight = 800;
-       const padding = 60;
+
+       /*
+       Data to use. topojson.feature creates a topojson object
+       */
+       var topojsonObject = topojson.feature(jsonCounty,jsonCounty.objects.counties);
+       const countiesDataset = topojsonObject.features;
 
        const svg = d3.select("#choropleth-map");
 
@@ -51,47 +54,30 @@ function main(jsonEducation, jsonCounty){
                      return (pageWidth - chartWidth)/2 + "px";
               })
               .style("margin-top", "20px");
-
-       /*Create projection - to project latitude and longitude coordinates on flat surface (monitor). Using standard d3 tool Mercator*/
-       var projekctija = d3.geoMercator()
-              /*center it*/
-              .translate([chartWidth/2,chartHeight/2]);
        /*
-         create a path (geoPath) using projection
+         using d.geoPath to generate "d" values for path elements. This will generate counties for the map.
        */
-      var path = d3.geoPath()
-              .projection(projekctija);
+      var path = d3.geoPath();
 
        svg.selectAll(".county")  
-              .data(topojson.feature(jsonCounty,jsonCounty.objects.counties).features)
+              .data(countiesDataset)
               .enter()
               .append("path")
               .attr("id", (d)=>{return d.id;})
-              .attr("fill", "none")
-              .attr("stroke", "black")
-              .attr("d", (d)=>{return generatePolyDrawingPath(d.geometry.coordinates);});
+              .attr("class", "county")
+              .attr("d", (d)=>{
+                     return path(d);
+              });
 
        /*https://github.com/topojson/topojson-client/blob/master/README.md#feature*/
        console.log("debug object");
-       console.log(topojson.feature(jsonCounty,jsonCounty.objects.counties).features);
+       console.log(countiesDataset);
        
        /*debug section*/
+       
        console.log("jsonEducation");
        console.log(jsonEducation);
        console.log("jsonCountry");
        console.log(jsonCounty);
 
-};
-
-function generatePolyDrawingPath(coordinateArray){
-
-       let startCoords = "M" + coordinateArray[0][0][0] + "," + coordinateArray[0][0][1] + " ";
-       let middleCoords = "";
-       let endPath = "z";
-
-       /*ignore first (starting position i=0) array and last (i=[].length since end position = starting position) array*/
-       for(let i=1; i<coordinateArray[0].length; i++){
-              middleCoords = middleCoords + "L" + coordinateArray[0][i][0] + ", " + coordinateArray[0][i][1] + " ";
-       };
-       return startCoords + middleCoords + endPath;
 };
