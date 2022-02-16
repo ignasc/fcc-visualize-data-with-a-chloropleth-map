6118612be017323412aa06d0e7907c8980c03022
ignasc <ignui.c@gmail.com>
2022-02-16 20:11:31 +0000

    Legend and color scaling added.
    Need to fix color range for percentages.

--- mainJS.js
+++ mainJS.js
@@ -62,9 +62,37 @@ function main(jsonEducation, jsonCounty){
               })
               .style("margin-top", "20px");
        /*
-         using d.geoPath to generate "d" values for path elements. This will generate counties for the map.
+         using d3.geoPath to generate "d" values for path elements. This "d" will generate counties borders for the map.
        */
       var path = d3.geoPath();
+
+      /*Setup data and variables for legend*/
+      var degreePercentMIN = 100;
+      var degreePercentMAX = 0;
+
+      jsonEducation.forEach(element => {
+             if(element.bachelorsOrHigher<degreePercentMIN){
+              degreePercentMIN=element.bachelorsOrHigher;
+             } else if (element.bachelorsOrHigher>degreePercentMAX) {
+              degreePercentMAX=element.bachelorsOrHigher;
+             };
+      });
+
+       const colorScaleX = d3.scaleLinear()
+                            .domain([degreePercentMIN, degreePercentMAX])
+                            /*range values for hsl() for light blue to saturated blue*/
+                            .range([80, 50]);
+       const legendAxisTicks = 10;
+       const legendChartWidth = Math.round(chartWidth*0.3);
+       const legendBarWidth = legendChartWidth/legendAxisTicks;
+       const legendBarHeight = 20;
+
+       const legendAxisScale = d3.scaleLinear()
+                                   .domain([degreePercentMIN, degreePercentMAX])
+                                   /*legend width is 25% of chart width*/
+                                   .range([0, chartWidth*0.25]);
+
+      console.log("Min value: " + degreePercentMIN + "</br>" + "Max value: " + degreePercentMAX);
 
       /*Create a <g> element to store the map in it*/
       svg.append("g")
@@ -86,6 +114,9 @@ function main(jsonEducation, jsonCounty){
               .attr("data-education", (d,index)=>jsonEducation[index].bachelorsOrHigher + "%")
               .attr("d", (d)=>{
                      return path(d);
+              })
+              .style("fill", (d,index)=>{
+                     return "hsl(235,100%," + colorScaleX(jsonEducation[index].bachelorsOrHigher) + "%)";
               })
               .on("mouseover", (pelesEvent)=>{
                      toolTip
@@ -135,11 +166,55 @@ function main(jsonEducation, jsonCounty){
 
        /*Add chart title*/
        svg.append("text")
-              .text("Title goes here")
+              .text("United States Education")
               .attr("x", chartWidth/2)
-              .attr("y", padding)
+              .attr("y", padding/2)
               .attr("id", "title")
               .attr("text-anchor", "middle");
+       svg.append("text")
+              .text("Percentage of adults holding Bachelor's or higher degree")
+              .attr("x", chartWidth/2)
+              .attr("y", padding)
+              .attr("id", "sub-title")
+              .attr("text-anchor", "middle");
+
+       /*Generate the legend*/
+
+       const xAxis = d3.axisBottom(legendAxisScale);
+
+       xAxis.ticks(legendAxisTicks)
+       .tickFormat(d=>d+"%");
+
+       /*Generate data array for legend bars*/
+       var legendData = [degreePercentMIN];
+       var step = (degreePercentMAX-degreePercentMIN)/legendAxisTicks;
+
+       for(let i = 0; i<legendAxisTicks-1;i++){
+              legendData.push(legendData[i]+step);
+       };
+
+       svg.append("g")
+       .attr("transform","translate(" + (chartWidth*0.6) + "," + (padding*1.5) + ")")
+       .attr("id", "axis-legend")
+       .call(xAxis);
+
+       console.log(legendData);
+       console.log(step);
+
+       /*Add color bars to the legend*/
+       svg.select("#axis-legend").selectAll("#legend-bars")
+       .data(legendData)
+       .enter()
+       .append("rect")
+       .attr("id", "legend-bar")
+       .attr("width", legendBarWidth)
+       .attr("height", legendBarHeight)
+       .style("fill", (d)=>{
+              return "hsl(235,100%," + colorScaleX(d) + "%)";
+       })
+       .attr("x", (d)=>legendAxisScale(d))
+       .attr("y",-legendBarHeight);
+
 
        /*https://github.com/topojson/topojson-client/blob/master/README.md#feature*/
        console.log("debug object");
